7287f5a2bf49bc08df9df97e9ad1713eee3f8350
Erhan Mutlu <erhan.mutlu@iyzico.com>
2019-01-01 15:02:06 +0300

    refactorings

---
+++ payment-consumer/src/main/java/org/erhanmutlu/payment/consumer/infrastructure/kafka/MessageProducerInterceptor.java
@@ -0,0 +1,42 @@
+package org.erhanmutlu.payment.consumer.infrastructure.kafka;
+
+import org.apache.kafka.clients.producer.ProducerInterceptor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class MessageProducerInterceptor implements ProducerInterceptor{
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageProducerInterceptor.class);
+
+    @Override
+    public ProducerRecord onSend(ProducerRecord record) {
+        if (logger.isDebugEnabled()) {
+            logger.debug(String.format("onSend topic=%s key=%s value=%s %d \n",
+                    record.topic(), record.key(), record.value().toString(),
+                    record.partition()
+            ));
+        }
+        return record;
+    }
+
+    @Override
+    public void onAcknowledgement(RecordMetadata metadata, Exception e) {
+        if (logger.isDebugEnabled()) {
+            logger.debug(String.format("onAck topic=%s, part=%d, offset=%d\n",
+                    metadata.topic(), metadata.partition(), metadata.offset()
+            ));
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+    }
+}
