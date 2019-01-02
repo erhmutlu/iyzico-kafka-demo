c7baab25c6593ec3e31014db0d42c7d2ec6acf79
Erhan Mutlu <erhan.mutlu@iyzico.com>
2019-01-02 14:12:14 +0300

    refactoring

--- payment-consumer/src/main/java/org/erhanmutlu/payment/consumer/infrastructure/kafka/CustomErrorHandler.java
+++ payment-consumer/src/main/java/org/erhanmutlu/payment/consumer/infrastructure/kafka/CustomErrorHandler.java
@@ -1,6 +1,7 @@
 package org.erhanmutlu.payment.consumer.infrastructure.kafka;
 
 import org.apache.kafka.clients.consumer.Consumer;
+import org.erhanmutlu.payment.common.IyzicoIdempotentMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.listener.ConsumerAwareListenerErrorHandler;
@@ -15,7 +16,9 @@ public class CustomErrorHandler implements ConsumerAwareListenerErrorHandler {
 
     @Override
     public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
-        logger.info("error! dıııııt", exception);
+        IyzicoIdempotentMessage payload = (IyzicoIdempotentMessage) message.getPayload();
+        logger.info("error! {}", payload.getUniqueId(), exception);
+
         MessageHeaders headers = message.getHeaders();
         consumer.seek(new org.apache.kafka.common.TopicPartition(
                         headers.get(KafkaHeaders.RECEIVED_TOPIC, String.class),
